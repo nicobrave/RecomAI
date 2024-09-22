f047bfe3ae1217271b5384ae4f18b30bed081197
nicobrave <nicobrave@icloud.com>
2024-09-22 10:31:18 -0300

    Configurar CORS para permitir solicitudes desde www.recomai.cl

--- app.py
+++ app.py
@@ -3,15 +3,13 @@ from flask_cors import CORS
 from pathlib import Path
 
 # Importa los blueprints
-from cliente2.cliente2_routes import cliente2_bp  # Asegúrate que el import está correcto
-from workid.workid_routes import workid_bp  # Asegúrate que el import está correcto
+from cliente2.cliente2_routes import cliente2_bp
+from workid.workid_routes import workid_bp
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
-
 app.template_folder = Path(app.template_folder).parent / 'templates'
 
-
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "https://www.recomai.cl"}})
 
 # Registrar los blueprints con sus respectivos prefijos
 app.register_blueprint(workid_bp, url_prefix='/workid')
@@ -22,5 +20,6 @@ app.register_blueprint(cliente2_bp, url_prefix='/cliente2')
 def home():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+# Elimina o comenta el bloque de ejecución
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8000, debug=True)
