afd77bb30f62793839e548338afae78020a987ab
nicobrave <nicobrave@icloud.com>
2024-09-22 10:47:07 -0300

    ultimos cambios 22/09

--- workid/workid_routes.py
+++ workid/workid_routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
+from flask_cors import cross_origin
+from workid.recommendations import generar_recomendacion_openai, df_trabajadores
 
 workid_bp = Blueprint('workid', __name__, template_folder='templates', static_folder='static')
 
@@ -6,3 +8,28 @@ workid_bp = Blueprint('workid', __name__, template_folder='templates', static_fo
 def home():
     return render_template('workid/index.html')
 
+@workid_bp.route('/recomendaciones', methods=['GET', 'OPTIONS'])
+@cross_origin(origins='https://www.recomai.cl')
+def obtener_recomendaciones():
+    if request.method == 'OPTIONS':
+        return '', 200
+
+    # Obtener los parámetros de la solicitud
+    cargo = request.args.get('cargo')
+    comuna = request.args.get('comuna')
+    nivel_estudio = request.args.get('nivel_estudio')
+    detalle_experiencia = request.args.get('detalle_experiencia')
+
+    if not cargo:
+        return jsonify({"error": "El parámetro 'cargo' es requerido"}), 400
+
+    # Llamar a la función para generar la recomendación
+    recomendacion = generar_recomendacion_openai(
+        df_trabajadores,
+        cargo=cargo,
+        comuna=comuna,
+        nivel_estudio=nivel_estudio,
+        detalle_experiencia=detalle_experiencia
+    )
+
+    return jsonify(recomendacion)
